92ab40b0061a749ad7cf5d1498d385d424b82509
Benjamin Jackson <benj@betterrentals.com.au>
2024-08-03 08:07:12 +1000

    completed getting started guide

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import _ from 'lodash';
+// import your function
+import myName from './myName';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  // use your function!
+  element.textContent = myName('Cody');
   return element;
 }
 
